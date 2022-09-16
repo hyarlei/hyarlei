5d0dc7d815297266aa52b6347b2e299151a85817
Hyarlei Silva <109905900+hyarlei@users.noreply.github.com>
2022-09-16 14:10:22 -0300

    Projeto Avião em Java

---
+++ aula04.java
@@ -0,0 +1,14 @@
+package aula04;
+
+public class aula04 {
+    public static void main(String[] args){
+        Aviao a1 = new Aviao();
+        a1.registro = 866464;
+        a1.nome = "baleia";
+        //c1.ponta = 0.5f;
+        a1.carga = 80;
+        a1.passageiros = 150; 
+        a1.status();
+    }
+    
+}
